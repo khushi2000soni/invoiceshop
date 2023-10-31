7dbffaed69823c2b041480fd02166e72768bfdc0
Khushi_soni <khushisoni.hipl@gmail.com>
2023-10-31 09:54:51 +0530

    active class in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,75 +7,93 @@
       </div>
       <ul class="sidebar-menu">
         <li class="menu-header">Main</li>
-        <li class="dropdown active">
-          {{-- <a href="{{ route('admin.dashboard') }}" class="nav-link "><i class="fas fa-home"></i><span>Dashboard</span></a> --}}
-          <a href="{{ route('dashboard') }}" class="nav-link "><i class="fas fa-home"></i><span>@lang('quickadmin.qa_dashboard')</span></a>
+        <li class="{{ Request::is('dashboard*') ? 'active' : '' }}">
+            <a href="{{ route('dashboard') }}" class="nav-link">
+                <i class="fas fa-home"></i><span>@lang('quickadmin.qa_dashboard')</span>
+            </a>
         </li>
+
         {{-- @can('role_access')
-        <li class="dropdown">
+        <li class="{{ Request::is('roles*') ? 'active' : '' }}">
             <a href="{{route('roles.index')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.roles.title')</span></a>
         </li>
         @endcan --}}
 
         @can('staff_access')
-        <li class="dropdown">
-            <a href="{{route('staff.index')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.user-management.title')</span></a>
+        <li class="{{ Request::is('staff*') ? 'active' : '' }}">
+            <a href="{{ route('staff.index') }}" class="nav-link">
+                <i class="fab fa-gg"></i><span>@lang('quickadmin.user-management.title')</span>
+            </a>
         </li>
         @endcan
+
         @can('customer_access')
-        <li class="dropdown">
-            <a href="{{route('customers.index')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.customer-management.title')</span></a>
+        <li class="{{ Request::is('customers*') ? 'active' : '' }}">
+            <a href="{{ route('customers.index') }}" class="nav-link">
+                <i class="fab fa-gg"></i><span>@lang('quickadmin.customer-management.title')</span>
+            </a>
         </li>
         @endcan
 
         @can('device_access')
-        <li class="dropdown">
-            <a href="{{route('device.index')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.device-management.title')</span></a>
+        <li class="{{ Request::is('device*') ? 'active' : '' }}">
+            <a href="{{ route('device.index') }}" class="nav-link">
+                <i class="fab fa-gg"></i><span>@lang('quickadmin.device-management.title')</span>
+            </a>
         </li>
         @endcan
 
         @can('invoice_access')
-        <li class="dropdown">
-            <a href="{{route('orders.index')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.order-management.title')</span></a>
+        <li class="{{ Request::is('orders*') ? 'active' : '' }}">
+            <a href="{{ route('orders.index') }}" class="nav-link">
+                <i class="fab fa-gg"></i><span>@lang('quickadmin.order-management.title')</span>
+            </a>
         </li>
         @endcan
 
         @can('report_access')
-        <li class="dropdown">
-            <a href="{{route('reports')}}" class="nav-link"><i class="fab fa-gg"></i><span>@lang('quickadmin.report-management.title')</span></a>
+        <li class="{{ Request::is('reports*') ? 'active' : '' }}">
+            <a href="{{ route('reports') }}" class="nav-link">
+                <i class="fab fa-gg"></i><span>@lang('quickadmin.report-management.title')</span>
+            </a>
         </li>
         @endcan
 
         <li class="dropdown">
-          <a href="#" class="nav-link has-dropdown"><i class="fab fa-gg"></i><span>@lang('quickadmin.master-management.title')</span></a>
-          <ul class="dropdown-menu">
-            @can('address_access')
-            <li><a class="nav-link" href="{{route('address.index')}}">@lang('quickadmin.address.title')</a></li>
-            @endcan
-            @can('category_access')
-            <li><a class="nav-link" href="{{route('categories.index')}}">@lang('quickadmin.category.title')</a></li>
-            @endcan
-            @can('product_access')
-            <li><a class="nav-link" href="{{route('products.index')}}">@lang('quickadmin.product-management.title')</a></li>
-            @endcan
-
-          </ul>
+            <a href="#" class="nav-link has-dropdown"><i class="fab fa-gg"></i><span>@lang('quickadmin.master-management.title')</span></a>
+            <ul class="dropdown-menu">
+                @can('address_access')
+                <li class="{{ Request::is('address*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('address.index') }}">@lang('quickadmin.address.title')</a>
+                </li>
+                @endcan
+                @can('category_access')
+                <li class="{{ Request::is('categories*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('categories.index') }}">@lang('quickadmin.category.title')</a>
+                </li>
+                @endcan
+                @can('product_access')
+                <li class="{{ Request::is('products*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('products.index') }}">@lang('quickadmin.product-management.title')</a>
+                </li>
+                @endcan
+            </ul>
         </li>
 
         @can('setting_access')
-        <li class="dropdown">
-            <a href="{{route('settings')}}" class="nav-link"><i class="fas fa-cog"></i><span>@lang('quickadmin.settings.title')</span></a>
+        <li class="{{ Request::is('settings*') ? 'active' : '' }}">
+            <a href="{{ route('settings') }}" class="nav-link">
+                <i class="fas fa-cog"></i><span>@lang('quickadmin.settings.title')</span>
+            </a>
         </li>
         @endcan
-        {{-- <li class="dropdown">
-          <a href="#" class="nav-link has-dropdown"><i class="fab fa-gg"></i><span>Employee</span></a>
-          <ul class="dropdown-menu">
-            <li><a class="nav-link" href="{{ route('employees.create') }}">Add Employee</a></li>
-            <li><a class="nav-link" href="{{ route('employees.index') }}">Employee List</a></li>
-          </ul>
-        </li> --}}
-        <li><a class="nav-link" href="{{ route('logout') }}"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a></li>
 
-      </ul>
+        <li class="{{ Request::is('logout*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('logout') }}">
+                <i class="fas fa-sign-out-alt"></i><span>Logout</span>
+            </a>
+        </li>
+    </ul>
+
     </aside>
   </div>
