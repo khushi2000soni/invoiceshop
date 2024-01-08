0d01f1ccd9a67767c60ecb1ae8183f43115434fa
Khushi_soni <khushisoni.hipl@gmail.com>
2024-01-08 12:19:20 +0530

    Set header issue in order print

--- resources/views/admin/order/print-orders-list.blade.php
+++ resources/views/admin/order/print-orders-list.blade.php
@@ -36,17 +36,17 @@
 @section('content')
     <div class="page-header">
         <header style="padding: 1px 0; max-width: 100%; margin: 0 auto;">
-            <h2 style="margin: 0;color: #2a2a33;font-size: 20px;font-weight: bold; text-align:center;"><strong>@lang('quickadmin.order-management.fields.list')</strong></h2>
+            <h2 style="margin: 0;color: #2a2a33;font-size: 18px;font-weight: bold; text-align:center;"><strong>@lang('quickadmin.order-management.fields.list')</strong></h2>
         </header>
     </div>
     <main class="main" style="max-width: 100%;margin: 0 auto;padding: 40px 0;padding-top: 20px;">
-        <table cellpadding="0" cellspacing="0" width="100%" style="color: #000;font-size: 16px;">
+        <table cellpadding="0" cellspacing="0" width="100%" style="color: #000;font-size: 15px;">
             <tbody>
                 <tr>
-                    <td colspan="4"><div class=""style="color: #2a2a33;font-size: 20px; text-align:left;">@lang('quickadmin.order.fields.duration') : {{ $duration}}</div></td>
+                    <td colspan="4"><div class=""style="color: #2a2a33;font-size: 16px; text-align:left;">@lang('quickadmin.order.fields.duration') : {{ $duration}}</div></td>
                 </tr>
                 <tr>
-                    <td colspan="4" style="padding-bottom: 20px"><div class="" style="color: #2a2a33;font-size: 20px; text-align:left;">@lang('quickadmin.order.fields.customer') : {{ $customer_name }}</div></td>
+                    <td colspan="4" style="padding-bottom: 20px"><div class="" style="color: #2a2a33;font-size: 16px; text-align:left;">@lang('quickadmin.order.fields.customer') : {{ $customer_name }}</div></td>
                 </tr>
                 <tr>
                     <th style="padding: 8px;border: 1px solid #000;border-right: none;" align="left">@lang('quickadmin.qa_sn')</th>
@@ -61,7 +61,7 @@
                     <td style="padding: 8px;border: 1px solid #000;border-right: none;" align="left">{{ $key + 1 }}</td>
                     <td style="padding: 8px;border: 1px solid #000;border-right: none;" align="center">{{  $order->invoice_number ?? '' }}</td>
                     <td style="padding: 8px;border: 1px solid #000;border-right: none;" align="center">{{ $order->customer ? $order->customer->full_name : ''}}</td>
-                    <td style="padding: 8px;border: 1px solid #000;border-right: none;" align="center">{{ $order->created_at->format('d-m-Y') }}</td>
+                    <td style="padding: 8px;border: 1px solid #000;border-right: none;" align="center">{{ $order->created_at->format('d-m-y') }}</td>
                     <td style="padding: 8px;border: 1px solid #000;" align="center">{{ $order->grand_total ?? '0'}}</td>
                 </tr>
                 @empty
@@ -70,8 +70,8 @@
                 </tr>
                 @endforelse
                 <tr>
-                    <th colspan="4" style="padding: 8px;border: 1px solid #000;border-right: none;border-top: none;" align="right">@lang('quickadmin.order.fields.grand_total')</th>
-                    <th style="padding: 8px 2px;border: 1px solid #000;border-top: none;" align="center">{{ $sumGrandTotal }}</th>
+                    <th colspan="4" style="padding: 8px;border: 1px solid #000;border-right: none;" align="right">@lang('quickadmin.order.fields.grand_total')</th>
+                    <th style="padding: 8px 2px;border: 1px solid #000;" align="center">{{ $sumGrandTotal }}</th>
                 </tr>
             </tbody>
         </table>
