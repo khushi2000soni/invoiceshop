9e6708c154bf336e2732fe8f1d16d45669fa88f4
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-01-29 13:13:10 +0530

    command correction of backup module

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -50,9 +50,11 @@ class DatabaseBackUp extends Command
             // $command = 'mysqldump --user=' . $username . ' --password=' . $password . ' --host=' . $host . ' --databases ' . $database . ' > ' . $backupPath . $fileName;
             $deleteTables= config('db_backup_tables.delete_tables');
             // command for taking backup of specific tables only
-            $command = 'mysqldump --user=' . $username . ' --password=' . $password . ' --host=' . $host . ' ' . $database . ' ' . implode(' ', $deleteTables) . ' > ' . $backupPath . $fileName;
+           // $command = 'mysqldump --user=' . $username . ' --password=' . $password . ' --host=' . $host . ' ' . $database . ' ' . implode(' ', $deleteTables) . ' > ' . $backupPath . $fileName;
+           $command = 'mysqldump -u '.$username.' --password='.$password.' -h '.$host.' '.$database.' --no-tablespaces '.implode(' ', $deleteTables).' > '.$backupPath.$fileName;
             $returnVar = null;
             $output = null;
+            //dd($command);
             exec($command, $output, $returnVar);
             if ($returnVar !== 0) {
                 throw new \Exception('mysqldump command failed: ' . implode(PHP_EOL, $output));
